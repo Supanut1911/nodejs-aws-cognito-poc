0b3af11f8b41977893a39c08f20bda47846f81a9
Supanut1911 <ynotmela@gmail.com>
2023-06-04 16:47:49 +0700

    refactor index

--- index.js
+++ index.js
@@ -1,74 +1,28 @@
-const AwsConfig = require("./AWSConfig");
-
-function signUp(email, password, agent = "none") {
-  return new Promise((resolve) => {
-    AwsConfig.initAWS();
-    AwsConfig.setCognitoAttributeList(email, agent);
-    AwsConfig.getUserPool().signUp(
-      email,
-      password,
-      AwsConfig.getCognitoAttributeList(),
-      null,
-      function (err, result) {
-        if (err) {
-          return resolve({ statusCode: 422, response: err });
-        }
-        const response = {
-          username: result.user.username,
-          userConfirmed: result.userConfirmed,
-          userAgent: result.user.client.userAgent,
-        };
-        return resolve({ statusCode: 201, response: response });
-      }
-    );
-  });
-}
-
-function verify(email, code) {
-  return new Promise((resolve) => {
-    AwsConfig.getCognitoUser(email).confirmRegistration(
-      code,
-      true,
-      (err, result) => {
-        if (err) {
-          return resolve({ statusCode: 422, response: err });
-        }
-        return resolve({ statusCode: 400, response: result });
-      }
-    );
-  });
-}
-
-function signIn(email, password) {
-  return new Promise((resolve) => {
-    AwsConfig.getCognitoUser(email).authenticateUser(
-      AwsConfig.getAuthDetails(email, password),
-      {
-        onSuccess: (result) => {
-          const token = {
-            accessToken: result.getAccessToken().getJwtToken(),
-            idToken: result.getIdToken().getJwtToken(),
-            refreshToken: result.getRefreshToken().getToken(),
-          };
-          return resolve({
-            statusCode: 200,
-            response: AwsConfig.decodeJWTToken(token),
-          });
-        },
-
-        onFailure: (err) => {
-          return resolve({
-            statusCode: 400,
-            response: err.message || JSON.stringify(err),
-          });
-        },
-      }
-    );
-  });
-}
-
-module.exports = {
-  signUp,
-  verify,
-  signIn,
+("use strict");
+global.fetch = require("node-fetch");
+require("dotenv").config();
+const Cognito = require("./cognito/index");
+const { verify } = require("./cognito/index");
+const body = {
+  email: "onexlab.io@gmail.com",
+  password: "Test123456!",
 };
+
+async function Signup() {
+  const response = await Cognito.signUp(body.email, body.password);
+  console.log(response);
+}
+
+async function Verify() {
+  const response = await Cognito.verify(body.email, "654769");
+  console.log(response);
+}
+
+async function SignIn() {
+  const response = await Cognito.signIn(body.email, body.password);
+  console.log(response);
+}
+
+// Signup();
+// Verify();
+// SignIn();
